26b75057c37e0c19bdb4011b7a54f8629ac7f3fe
rguarnizo <rguarnizo@unal.edu.co>
2022-04-15 23:02:57 -0500

    Adding example of using of DB

--- statera-backend/main.go
+++ statera-backend/main.go
@@ -1,19 +1,30 @@
 package main
 
 import(
-	//"github.com/jurodriguezf/statera/cmd/server"
+	"github.com/jurodriguezf/statera/cmd/server"
 	"github.com/jurodriguezf/statera/cmd/db"
+	//! Descomentar esta importación para probar el ejemplo
+    //"go.mongodb.org/mongo-driver/bson"
+
 )
 
 func main() {
-	client, ctx, cancel, err := connection.Connect("mongodb+srv://admin:admin@staterabd.brnsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	client, ctx, cancel, err := connection.Connect("mongodb+srv://admin:admin@stateradb.brnsm.mongodb.net/StateraDB?retryWrites=true&w=majority")
 	if err != nil{
         panic(err)
     }
-	//server.SetupEndpoints()
 
-	defer connection.Close(client, ctx, cancel);
+	defer connection.Close(client, ctx, cancel)
 
 	connection.Ping(client, ctx)
+
+	// Ejemplo de inserción de una receta en la base de datos:
+	// db := client.Database("StateraDB")
+	// recipes := db.Collection("Recipes")
+
+	// recipes.InsertOne(ctx,bson.D{{"name","Papitas sin sal"},{"ingredientes",bson.A{"Papitas","Sal"}}});
+
+	server.SetupEndpoints()
+
 
 }
