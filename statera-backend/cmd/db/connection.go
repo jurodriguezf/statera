0428dafa86e4a787ee4ffd8f00444e86f5df1a80
rguarnizo <rguarnizo@unal.edu.co>
2022-04-16 22:08:08 -0500

    Do the same but with connection file XD

--- statera-backend/cmd/db/connection.go
+++ statera-backend/cmd/db/connection.go
@@ -2,55 +2,52 @@ package connection
 
 import (
 	"context"
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+func Connect(uri string) (*mongo.Client, context.Context,
+	context.CancelFunc, error) {
 
-func Connect(uri string)(*mongo.Client, context.Context,
-                          context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 
-	ctx, cancel := context.WithTimeout(context.Background(),30*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 
-
-	client, err :=  mongo.Connect(ctx,options.Client().ApplyURI(uri))
-
-	return client,ctx,cancel,err
+	return client, ctx, cancel, err
 
 }
 
+func Ping(client *mongo.Client, ctx context.Context) error {
 
-func Ping(client *mongo.Client, ctx context.Context) error{
-
-    // mongo.Client has Ping to ping mongoDB, deadline of
-    // the Ping method will be determined by cxt
-    // Ping method return error if any occurred, then
-    // the error can be handled.
-    if err := client.Ping(ctx, readpref.Primary()); err != nil {
-        return err
-    }
-    fmt.Println("connected successfully")
-    return nil
+	// mongo.Client has Ping to ping mongoDB, deadline of
+	// the Ping method will be determined by cxt
+	// Ping method return error if any occurred, then
+	// the error can be handled.
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected successfully")
+	return nil
 }
 
 func Close(client *mongo.Client, ctx context.Context,
-           cancel context.CancelFunc){
+	cancel context.CancelFunc) {
 
-    // CancelFunc to cancel to context
-    defer cancel()
+	// CancelFunc to cancel to context
+	defer cancel()
 
-    // client provides a method to close
-    // a mongoDB connection.
-    defer func(){
+	// client provides a method to close
+	// a mongoDB connection.
+	defer func() {
 
-        // client.Disconnect method also has deadline.
-        // returns error if any,
-        if err := client.Disconnect(ctx); err != nil{
-            panic(err)
-        }
-    }()
+		// client.Disconnect method also has deadline.
+		// returns error if any,
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
