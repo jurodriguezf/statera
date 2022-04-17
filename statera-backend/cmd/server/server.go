0866f3cc93e3a3a70b7b371b5f89b940fbd8d637
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-04-16 20:50:08 -0500

    change goodbye for hello

--- statera-backend/cmd/server/server.go
+++ statera-backend/cmd/server/server.go
@@ -14,7 +14,7 @@ func SetupEndpoints() {
 	})
 
 	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "goodbye")
+		c.JSON(http.StatusOK, "hello")
 	})
 
 	screenEndpoints := router.Group("/")
