314be0e7b75b9bb499b7b8727662edbf98efb6bf
rguarnizo <rguarnizo@unal.edu.co>
2022-04-24 17:18:30 -0500

    Refactor Login and SignUp in various components

--- statera-frontend/src/pages/login/Login.jsx
+++ statera-frontend/src/pages/login/Login.jsx
@@ -8,40 +8,52 @@ import {Link} from "react-router-dom";
 const Login = (props) => {
   return (
     <div {...props} className="flex">
-      <div className="relative hidden md:block w-full h-screen object-fill ">
-        <div className="absolute flex items-center w-full px-10">
-          <img src="logo.png" className="object-cover h-24"></img>
-          <p className="font-youngserif text-wine text-xl"> Statera</p>
-        </div>
-        <img src="images/bag-vegetals.jpg" alt="" className="h-full w-full object-cover" />
-      </div>
+      <Image/>
       <div className="my-auto px-20 w-full">
-        <div className="flex justify-end">
-          <p className="font-youngserif text-sm mt-10">
-            Don’t have an account?
-            <Link to="/signUp" className="text-wine px-1">
-              Sign up for free
-            </Link>
-          </p>
-        </div>
+        <SignUpLink/>
         <p className="font-youngserif text-4xl my-10">Welcome back!</p>
-        <form className="">
-          <Input title="Email" />
-          <Input title="Password" password />
-          <div className="flex justify-end mt-2">
-            <a href="" className="font-youngserif text-sm">
-              Forgot password?
-            </a>
-          </div>
-          <div className="w-full flex justify-center my-10">
-            <PrimaryButton label="Sign in" className="" />
-          </div>
-        </form>
+        <LoginForm/>
         <DivisorLine />
         <GoogleButton label="Continue with Google" />
       </div>
     </div>
   );
+
+  function LoginForm() {
+    return <form className="">
+      <Input title="Email" />
+      <Input title="Password" password />
+      <div className="flex justify-end mt-2">
+        <a href="" className="font-youngserif text-sm">
+          Forgot password?
+        </a>
+      </div>
+      <div className="w-full flex justify-center my-10">
+        <PrimaryButton label="Sign in" className="" />
+      </div>
+    </form>;
+  }
+
+  function SignUpLink() {
+    return <div className="flex justify-end">
+      <p className="font-youngserif text-sm mt-10">
+        Don’t have an account?
+        <Link to="/signUp" className="text-wine px-1">
+          Sign up for free
+        </Link>
+      </p>
+    </div>;
+  }
+
+  function Image() {
+    return <div className="relative hidden md:block w-full h-screen object-fill ">
+      <div className="absolute flex items-center w-full px-10">
+        <img src="logo.png" className="object-cover h-24"></img>
+        <p className="font-youngserif text-wine text-xl"> Statera</p>
+      </div>
+      <img src="images/bag-vegetals.jpg" alt="" className="h-full w-full object-cover" />
+    </div>;
+  }
 };
 
 export default Login;
--- statera-frontend/src/pages/signUp/SignUp.jsx
+++ statera-frontend/src/pages/signUp/SignUp.jsx
@@ -9,40 +9,52 @@ const SignUp = (props) => {
   return (
     <div {...props} className="flex">
       <div className="relative w-full my-auto px-10 md:px-20 sm:w-full">
-        <p className="font-youngserif text-sm my-10">
-          Already have an account?
-          <Link to="/login" className="text-wine px-1">
-            Login
-          </Link>
-        </p>
+        <LoginLink/>
         <p className="font-youngserif text-4xl my-10">Create an account</p>
-        <form className="">
-          <Input title="Username" />
-          <Input title="Email" />
-          <div className="flex justify-around">
-            <div className="w-3/6">
-              <Input title="Password" password />
-            </div>
-            <div className="w-3/6">
-              <Input title="Confirm" password />
-            </div>
-          </div>
-          <div className="w-full flex justify-center my-10">
-            <PrimaryButton label="Sign up" className="" />
-          </div>
-        </form>
+        <LoginForm/>
         <DivisorLine />
         <GoogleButton label="Continue with Google" />
       </div>
-      <div className="relative hidden md:block w-full h-screen ">
+      <Image/>
+    </div>
+  );
+
+  function Image() {
+    return <div className="relative hidden md:block w-full h-screen ">
       <div className="absolute flex items-center justify-end w-full px-10">
         <img src="logo.png" className="object-scale-down h-24"></img>
         <p className="font-youngserif text-wine text-xl"> Statera</p>
       </div>
-        <img src="images/vegetals.jpg" alt="" className="object-cover h-full" />
+      <img src="images/vegetals.jpg" alt="" className="object-cover h-full" />
+    </div>;
+  }
+
+  function LoginForm() {
+    return <form className="">
+      <Input title="Username" />
+      <Input title="Email" />
+      <div className="flex justify-around">
+        <div className="w-3/6">
+          <Input title="Password" password />
+        </div>
+        <div className="w-3/6">
+          <Input title="Confirm" password />
+        </div>
       </div>
-    </div>
-  );
+      <div className="w-full flex justify-center my-10">
+        <PrimaryButton label="Sign up" className="" />
+      </div>
+    </form>;
+  }
+
+  function LoginLink() {
+    return <p className="font-youngserif text-sm my-10">
+      Already have an account?
+      <Link to="/login" className="text-wine px-1">
+        Login
+      </Link>
+    </p>;
+  }
 };
 
 export default SignUp;
