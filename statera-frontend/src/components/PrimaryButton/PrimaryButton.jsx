c9ab9c51f909bae2ef077a4eedded1d0148379a0
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-05-10 02:01:02 -0500

    fix button functionality for submit links

--- statera-frontend/src/components/PrimaryButton/PrimaryButton.jsx
+++ statera-frontend/src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const PrimaryButton = (props) => {
-  return <button type={props.type} onClick={props.onClick} className="btn-primary">{props.label}</button>;
+  const navigate = useNavigate();
+
+  return <button type={props.type} onClick={props.link ? (() => navigate(props.link)) : (() => {})} className="btn-primary">{props.label}</button>;
 };
 
 export default PrimaryButton;
