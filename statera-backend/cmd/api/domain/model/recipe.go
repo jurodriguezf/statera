fbd2532649e1990fd1a88377d2898d7513a61045
Brayan Leon <bdleons@unal.edu.co>
2022-05-09 23:43:49 -0500

    STTR-46 Agregado campo 'instrucciones' a las recetas

--- statera-backend/cmd/api/domain/model/recipe.go
+++ statera-backend/cmd/api/domain/model/recipe.go
@@ -6,7 +6,8 @@ import (
 
 /*Recipe is the model of recipe in MongoDB*/
 type Recipe struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name        string             `bson:"name" json:"name,omitempty"`
-	Ingredients []string           `bson:"ingredients" json:"ingredients,omitempty"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name         string             `bson:"name" json:"name,omitempty"`
+	Ingredients  []string           `bson:"ingredients" json:"ingredients,omitempty"`
+	Instructions []string           `bson:"instructions" json:"instructions,omitempty"`
 }
