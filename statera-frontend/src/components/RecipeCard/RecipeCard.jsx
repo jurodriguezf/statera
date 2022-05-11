5b0146d458a3b17d2a72067ad957ec7b610281c0
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-05-11 02:09:54 -0500

    add home screen cards grid support

--- statera-frontend/src/components/RecipeCard/RecipeCard.jsx
+++ statera-frontend/src/components/RecipeCard/RecipeCard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const RecipeCard = (props) => {
 
   return (
-    <div className="p-3 m-5">
+    <div>
       <div >
-        <img className="rounded-3xl" src="https://via.placeholder.com/350x200"/>
+        <img className="rounded-3xl drop-shadow-lg" src="https://via.placeholder.com/350x200"/>
       </div>
       <div className="m-3">
         <div className="font-bold font-manrope text-xl ">{props.name}</div>
--- statera-frontend/src/pages/home/Home.jsx
+++ statera-frontend/src/pages/home/Home.jsx
@@ -6,13 +6,24 @@ import Panel from "../../layout/BasicLayout/Panel";
 const Home = () => {
   return (
     <Panel>
-        This is a homepage, it means you are successfully logged in
-
-        <div className="flex flex-row">
-          <PrimaryButton label="Ir a mi perfil" link={"/my-profile"}/>
+      <div className="px-10">
+        <div className={"font-youngserif text-5xl leading-normal mt-2 sm:mt-10 mb-4"}>
+          <h1>Bienvenido!</h1>
+        </div>
+        <div className={"font-manrope font-bold text-2xl leading-normal mt-2 sm:mt-10 mb-4"}>
+          <h1>Recetas populares</h1>
+        </div>
+        <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center">
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
+          <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
           <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
           <RecipeCard name="Fríjoles con kumis" category="Ensalada"/>
         </div>
+      </div>
     </Panel>
   )
 }
