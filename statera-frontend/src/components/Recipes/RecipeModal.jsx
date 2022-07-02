b29587e1eb9dae8d118e3a319c9b9841f176b723
Brayan Leon <bdleons@unal.edu.co>
2022-07-02 17:40:22 -0500

    Misc json

--- statera-frontend/src/components/Recipes/RecipeModal.jsx
+++ statera-frontend/src/components/Recipes/RecipeModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import LikeButton from "../LikeButton/LikeButton";
 
 import CommentAndRating from "../Comments/CommentAndRating";
